d58ec08734838279034b5ff79b425715cfe13ff4
Ashoka Bhat <164073554+ASHOKABHAT69@users.noreply.github.com>
2024-11-24 17:18:58 +0530

    pull update

--- Welcum.java
+++ Welcum.java
@@ -3,7 +3,6 @@ public class GIT
     public static void main (String args[])
     {
       System.out.println("WELCOME TO YOUR GIT JAVA FILE");
-      System.out.println("6106");
       System.out.println("Acer Swift");
       System.out.println("GP 14");
     }
